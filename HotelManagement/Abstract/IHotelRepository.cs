e6d02599a58b314ab337d21923f089e00a6c85d2
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-02-25 17:57:58 +0300

    Interface

---
+++ HotelManagement/Abstract/IHotelRepository.cs
@@ -0,0 +1,16 @@
+﻿using HotelManagement.Entities;
+
+namespace HotelManagement.Abstract
+{
+    public interface IHotelRepository
+    {
+
+        List<Hotel>GetAllHotels();
+        Hotel GetHotelById(int id);
+
+        Hotel createHotel(Hotel hotel);
+        Hotel UpdateHotel(Hotel hotel);
+        Hotel DeleteHotel(int id);
+
+    }
+}
--- HotelManagement/Program.cs
+++ HotelManagement/Program.cs
@@ -1,6 +1,12 @@
+using HotelManagement.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<HotelDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
