307767e1c20e9c0514be20192df9692b03d896fa
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-02-25 17:44:42 +0300

    Entites

--- HotelManagement/Entities/Hotel.cs
+++ HotelManagement/Entities/Hotel.cs
@@ -1,6 +1,21 @@
-﻿namespace HotelManagement.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelManagement.Entities
 {
     public class Hotel
     {
+        //Id alanını primary key olarak verdim ve bu değerin birer birer artmasını istiyorum
+
+        [Key , DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [StringLength(50)] 
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string City { get; set; }
+
+
     }
 }
