158f4bc8bb414d1f29e519367caca2c1ed32d91b
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-02-25 18:07:42 +0300

    Interfaces

---
+++ HotelManagement/Business/Abstract/IHotelService.cs
@@ -0,0 +1,16 @@
+﻿using HotelManagement.Entities;
+
+namespace HotelManagement.Business.Abstract
+{
+    public interface IHotelService
+    {
+
+        List<Hotel> GetlAllHotels();
+        Hotel GetHotelById(int id);
+        Hotel CreateHotel(Hotel hotel);
+        Hotel UpdateHotel(Hotel hotel);
+        Hotel DeleteHotel(int id);
+
+
+    }
+}
