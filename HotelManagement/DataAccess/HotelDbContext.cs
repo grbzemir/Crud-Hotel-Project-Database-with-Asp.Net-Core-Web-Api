85d3fda53bfc4344ab28e0632e0e7c5c1d0c20b3
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-02-25 17:57:45 +0300

    Database

---
+++ HotelManagement/DataAccess/HotelDbContext.cs
@@ -0,0 +1,33 @@
+﻿using HotelManagement.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelManagement.DataAccess
+{
+    public class HotelDbContext:DbContext
+    {
+        public HotelDbContext(DbContextOptions<HotelDbContext>options):base(options)
+        {
+            
+        }
+
+        public HotelDbContext()
+        {
+            
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                string connectionString = configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+    }
+}
